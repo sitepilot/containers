09c2d3945b7bb8d5073a671c6a6b5fa476b00b07
Nick Jansen <n.jansen@sitepilot.io>
2024-05-01 10:19:30 +0200

    Refactor logging and directory creation
    Unified the log directories for each service under the "RUNTIME_LOGS_DIR" environment variable and removed the previously used logging directories. Simplified the directory creation in the entrypoint scripts to reduce redundancy. Also added a new environment variable in the Dockerfile to accommodate the changes.

--- src/php-nginx/runtime/entrypoint.d/php-nginx.sh
+++ src/php-nginx/runtime/entrypoint.d/php-nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p "$NGINX_LOGS_DIR" "$NGINX_CERTS_DIR"
+mkdir -p "$NGINX_CERTS_DIR"
 
 template nginx.conf.tmpl /etc/nginx/nginx.conf
 template nginx-app.conf.tmpl /etc/nginx/sites-enabled/app.conf
--- src/php-ols/runtime/entrypoint.d/php-ols.sh
+++ src/php-ols/runtime/entrypoint.d/php-ols.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p "$OLS_LOGS_DIR" "$OLS_CERTS_DIR"
+mkdir -p "$OLS_CERTS_DIR"
 
 template ols.conf.tmpl /usr/local/lsws/conf/httpd_config.conf
 template ols-app.conf.tmpl /usr/local/lsws/conf/vhosts/app.conf
