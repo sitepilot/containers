38113700d9cb7f9c91bb8ffb27d64d20116f602f
Nick Jansen <n.jansen@sitepilot.io>
2024-09-17 16:11:17 +0200

    Include 'src/runtime/**' in workflows and enhance cron setup.
    Added 'src/runtime/**' to the paths in multiple GitHub workflows to ensure they're triggered by changes in the runtime directory. Additionally, improved the cron setup by adding a crontab command in the entrypoint script and simplified the Dockerfile environment variables.

--- src/runtime/runtime/entrypoint.d/40-cron.sh
+++ src/runtime/runtime/entrypoint.d/40-cron.sh
@@ -9,6 +9,7 @@ if [ -n "$RUNTIME_CRONTAB" ]; then
   info "Cron: Enabled"
   touch  /etc/s6-overlay/s6-rc.d/user/contents.d/cron
   template crontab.tmpl /var/spool/cron/crontabs/"$RUNTIME_USER"
+  crontab /var/spool/cron/crontabs/"$RUNTIME_USER"
 else
   info "Cron: Disabled"
 fi
